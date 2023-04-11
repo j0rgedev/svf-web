d9bceb6bc72ff352ec5b674dd85472b65adc58c3
james <james.777xd@outlook.com>
2023-04-11 18:20:56 -0500

    h1

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 function App() {
   return (
     <div className="App">
+      <h1>Matricula</h1>
       <li>
         <ul>Nombre</ul>
         <ul>Apellido</ul>
         <ul>DNI</ul>
         <ul>Edad</ul>
       </li>
+      <p>Registrarse</p>
     </div>
   )
 }
