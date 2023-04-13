c8b814215c4d0158bf7ebd7e4732cd56a530a163
james <james.777xd@outlook.com>
2023-04-13 10:37:12 -0500

    Estructura del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,7 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <h1>Matricula</h1>
-      <li>
-        <ul>Nombre</ul>
-        <ul>Apellido</ul>
-        <ul>DNI</ul>
-        <ul>Edad</ul>
-      </li>
-      <p>Registrarse</p>
+      
     </div>
   )
 }
