293cf8a5cdb206fd8771f635179a22a17df76e93
Jorge Antezana <102924003+jorgevfx@users.noreply.github.com>
2023-05-17 16:44:20 -0500

    Conflicts fix

--- src/app/enrollment-system/config/sms-validationSchema.js
+++
@@ -1,9 +0,0 @@
-import * as yup from 'yup'
-import { smsCodeRegex } from './regexFields.js'
-
-export const smsvalidationSchema = yup.object().shape({
-    code_sms: yup
-        .string()
-        .matches(smsCodeRegex, {message: 'El código es inválido'})
-        .required('El código es requerido'),
-})
